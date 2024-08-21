c8018101f611214c8ca0bd0b98fe1f0936a967ac
hong <etkimhong@gmail.com>
2024-08-22 01:22:23 +0700

    five

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-    <div className="min-h-full max-w-full bg-green-600 ">
-      <h1>hello!</h1>
-    </div>
-   </>
+    <>
+      <div className="min-h-full max-w-full bg-green-600 ">
+        <h2>what</h2>
+      </div>
+    </>
   );
 }
