06a7c06f9f48591dee81648aa0e43c2b247966cd
hong <etkimhong@gmail.com>
2024-08-22 09:50:29 +0700

    twel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["..//*.mdx", "..//*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../app/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
